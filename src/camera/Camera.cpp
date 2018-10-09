df7806aadb0181aa648dcda6e3f308bcb83064b3
Jean-Baptiste Lorteau <jb.lort@gmail.com>
2018-10-09 15:06:11 +0200

    Improved camera, added lookAt matrix

--- src/camera/Camera.cpp
+++ src/camera/Camera.cpp
@@ -1,5 +1,7 @@
 #include "camera/Camera.h"
 
+#include "logging/Logger.h"
+
 #include <glm/gtc/matrix_transform.hpp>
 
 #include <cmath>
@@ -7,24 +9,21 @@
 using namespace rt;
 
 namespace {
-    glm::mat4 makeViewTransform(const glm::vec3& position, const glm::vec3& orientation) {
-        glm::mat4 viewTransform{1.0};
-
-        viewTransform = glm::translate(viewTransform, position);
-        viewTransform = glm::rotate(viewTransform, orientation.x, glm::vec3(1.0, 0.0, 0.0));
-        viewTransform = glm::rotate(viewTransform, orientation.y, glm::vec3(0.0, 1.0, 0.0));
-        viewTransform = glm::rotate(viewTransform, orientation.z, glm::vec3(0.0, 0.0, 1.0));
-
+    glm::mat4 makeViewTransform(const glm::vec3& position, const glm::vec3& lookAt) {
+        auto eye = position;
+        auto center = lookAt;
+        auto up = glm::vec3{0.0, 1.0, 0.0};
+        auto viewTransform = glm::lookAt(eye, center, up);
         return viewTransform;
     }
 }
 
-Camera::Camera(const glm::vec3& position, const glm::vec3& orientation):
-_viewTransform{makeViewTransform(position, orientation)},
+Camera::Camera(const glm::vec3& position, const glm::vec3& lookAt):
+_viewTransform{makeViewTransform(position, lookAt)},
 _position{position},
-_orientation{orientation},
-_fov{70.0},
-_fovFactor{1 / std::tan(_fov / 2.0)}
+_lookAt{lookAt},
+_fov{100.0},
+_fovFactor{std::tan(_fov * M_PI * 0.5 / 180.0)}
 {
 }
 
--- src/camera/Camera.h
+++ src/camera/Camera.h
@@ -7,7 +7,7 @@ namespace rt {
     class Camera {
     public:
         // Initializes a camera with default properties
-        Camera(const glm::vec3& position, const glm::vec3& orientation);
+        Camera(const glm::vec3& position, const glm::vec3& lootAt);
 
         glm::mat4 viewTransform() const;
         double    fovFactor() const;
@@ -16,7 +16,7 @@ namespace rt {
     private:
         glm::mat4 _viewTransform;
         glm::vec3 _position;
-        glm::vec3 _orientation;
+        glm::vec3 _lookAt;
         double    _fov;
         double    _fovFactor;
     };
