ba79489d8307a33e12807eeced0c21a3b59bf710
Jean-Baptiste Lorteau <jb.lort@gmail.com>
2018-10-09 15:56:26 +0200

    Using simple tracer to generate default image

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <argh.h>
 
 #include "image/ImageIO.h"
-#include "tracer/Tracer.h"
+#include "tracer/SimpleTracer.h"
 
 int main(int argc, char** argv) {
     using namespace rt;
 
-    auto image = raytracer::traceDefaultImage();
+    auto image = SimpleTracer::trace();
 
     ImageIO::saveToFile(image, "default.tga");
 
