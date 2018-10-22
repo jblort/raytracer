f325bd984781c535edbd98b621445a0c56353da0
Jean-Baptiste Lorteau <jb.lort@gmail.com>
2018-10-22 16:44:37 +0200

    Cleanup

--- src/traceables/shapes/Sphere.h
+++ src/traceables/shapes/Sphere.h
@@ -2,18 +2,12 @@
 
 #include <glm/glm.hpp>
 
+#include "materials/SimpleMaterial.h"
 #include "traceables/Traceable.h"
 
 namespace rt {
 
 // A default red material for spheres
-const SimpleMaterial DefaultMaterial = SimpleMaterial{
-                                           Color{0.3, 0.0, 0.0},
-                                           Color{0.7, 0.0, 0.0},
-                                           Color{1.0, 1.0, 1.0},
-                                           40.0
-                                       };
-
 class Sphere: public Traceable {
 public:
     Sphere(const glm::vec3& center, double radius, const SimpleMaterial& material = DefaultMaterial);
