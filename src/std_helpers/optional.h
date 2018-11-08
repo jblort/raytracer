57caf3c4774512b9897c87e4d203869d9e7efa2a
Jean-Baptiste Lorteau <jb.lort@gmail.com>
2018-11-08 16:13:14 +0100

    Fixed optional header

--- src/std_helpers/optional.h
+++ src/std_helpers/optional.h
@@ -3,9 +3,12 @@
 // OSX Clang doesn't support optional yet
 #ifdef __APPLE__
     #include <experimental/optional>
+    template<typename T>
+    using optional = std::experimental::optional<T>;
 #else
     #include <optional>
+    template<typename T>
+    using optional = std::optional<T>;
 #endif
 
-template<typename T>
-using optional = std::experimental::optional<T>;
+
