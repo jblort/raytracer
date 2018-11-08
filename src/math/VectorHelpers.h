1e915ae70c1e64527ca1a6766ddede14060b8e26
Jean-Baptiste Lorteau <jb.lort@gmail.com>
2018-11-08 18:03:01 +0100

    Use sqrt instead of sqrtf

--- src/math/VectorHelpers.h
+++ src/math/VectorHelpers.h
@@ -8,7 +8,7 @@ namespace rt {
 
     template<typename T>
     float norm(const T& vec) {
-        float sum = vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2];
-        return std::sqrtf(sum);
+        double sum = vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2];
+        return std::sqrt(sum);
     }
 }
