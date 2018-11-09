3b52077cc236a0c33a124ccd9dd75f0c24bbcb2c
Jean-Baptiste Lorteau <jb.lort@gmail.com>
2018-11-09 19:59:38 +0100

    Fixed floating point narrowing issues in Color

--- src/image/Color.h
+++ src/image/Color.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cmath>
 
 namespace rt {
@@ -26,15 +27,24 @@ Color operator*(const Color& lhs, const Color& rhs);
 Color operator-(const Color& lhs, const Color& rhs);
 
 template<typename T>
-Color operator*(const Color& color, const T& value) {
-    return Color{std::fmin(float(color.r() * value), 1.0),
-                 std::fmin(float(color.g() * value), 1.0),
-                 std::fmin(float(color.b() * value), 1.0),
-                 std::fmin(float(color.a() * value), 1.0)};
+inline Color operator*(const Color& color, const T& value) {
+    return Color{float(std::min(color.r() * value, 1.0)),
+                 float(std::min(color.g() * value, 1.0)),
+                 float(std::min(color.b() * value, 1.0)),
+                 float(std::min(color.a() * value, 1.0))};
+}
+
+template<>
+inline Color operator*<float>(const Color& color, const float& value) {
+    return Color{std::min(color.r() * value, 1.0f),
+                 std::min(color.g() * value, 1.0f),
+                 std::min(color.b() * value, 1.0f),
+                 std::min(color.a() * value, 1.0f)};
+
 }
 
 template<typename T>
-Color operator*(const T& value, const Color& color) {
+inline Color operator*(const T& value, const Color& color) {
     return color * value;
 }
 
