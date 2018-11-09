157e9bd1d884b217f4195e643ba79e96bec29e24
Jean-Baptiste Lorteau <jb.lort@gmail.com>
2018-11-09 17:39:28 +0100

    Added alias templates for smart pointers

--- src/std_helpers/std_helpers.h
+++ src/std_helpers/std_helpers.h
@@ -1,3 +1,11 @@
 #pragma once
 
 #include "std_helpers/optional.h"
+
+#include <memory>
+
+template<typename T>
+using sptr = std::shared_ptr<T>;
+
+template<typename T>
+using uptr = std::unique_ptr<T>;
